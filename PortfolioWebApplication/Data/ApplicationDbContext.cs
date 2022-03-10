583adc9b26d51cafb9eb117d59681994503afd94
unknown <tylaminovel@gmail.com>
2022-03-09 17:21:18 -0800

    added all files

---
+++ PortfolioWebApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PortfolioWebApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
